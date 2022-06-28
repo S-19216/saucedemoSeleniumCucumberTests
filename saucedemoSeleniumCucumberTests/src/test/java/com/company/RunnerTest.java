9b746ec1d4f90ca02559d0c7708f466a5b639d56
S-19216 <s19216@pjwstk.edu.pl>
2022-06-29 00:52:31 +0200

    fix3

--- saucedemoSeleniumCucumberTests/src/test/java/com/company/RunnerTest.java
+++ saucedemoSeleniumCucumberTests/src/test/java/com/company/RunnerTest.java
@@ -4,10 +4,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
-//        "junit:target/cucumber-reports/Cucumber.xml",
-//        "html:target/cucumber-report.html"}, features = "src/test/resources/features",
-//        tags = "not @ignore")
-//public class RunnerTest {
-//}
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
+        "junit:target/cucumber-reports/Cucumber.xml",
+        "html:target/cucumber-report.html"}, features = "src/test/resources/features",
+        tags = "not @ignore")
+public class RunnerTest {
+}
